b4871b3f6cbb018a6c77294a5410e81bc13ddf88
nceeoh7 <51413645+nceeoh7@users.noreply.github.com>
2023-01-18 02:39:50 +0000

    added more config

--- atm.c
+++ atm.c
@@ -6,6 +6,7 @@ int main()
 {
     //How to ask for the amount of money to dispense
     int i = get_int("Use me to ask for money\n");
+    printf("You asked for $%d\n", i);
 
     //get_string() will help you get a name
     //get_char() is a good way to ask if the machine should keep dispensing.
